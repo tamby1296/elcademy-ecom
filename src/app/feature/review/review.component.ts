0d71f2469326954a9a5c7235798636c88407864d
Tamby Tia RAZAFINDRALAMBO <tambytia.razafindralambo@elca.mu>
2025-06-10 12:06:56 +0400

    custom

--- src/app/feature/review/review.component.ts
+++ src/app/feature/review/review.component.ts
@@ -1,14 +1,13 @@
-import { Component } from '@angular/core';
-import { ReviewFormComponent } from '../../components/review-form/review-form.component';
-import { HeaderComponent } from '../../components/header/header.component';
+import { Component } from "@angular/core";
+import { ReviewFormComponent } from "../../components/review-form/review-form.component";
+import { HeaderComponent } from "../../components/header/header.component";
+import { CommonModule } from "@angular/common";
 
 @Component({
-  selector: 'app-review',
+  selector: "app-review",
   standalone: true,
-  imports: [ReviewFormComponent, HeaderComponent],
-  templateUrl: './review.component.html',
-  styleUrl: './review.component.scss'
+  imports: [CommonModule, ReviewFormComponent, HeaderComponent],
+  templateUrl: "./review.component.html",
+  styleUrl: "./review.component.scss",
 })
-export class ReviewComponent {
-
-}
+export class ReviewComponent {}
